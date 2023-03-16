ba907b2a6e11f4b48c38b5157d0eaf53a34f78c7
Justin Weisberg <juhstinn@gmail.com>
2023-03-15 19:50:14 -0700

    init commit

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+function LandingPage() {
+  return (
+    <div className="">
+<img src='main-page.jpg' alt="" className='w-screen absolute z-0 top-0'/>
+
+    </div>
+  )
+}
+
+export default LandingPage
