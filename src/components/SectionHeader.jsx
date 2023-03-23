bf12238fceb908d984c3d3001c84cade9d36cf62
Justin Weisberg <juhstinn@gmail.com>
2023-03-23 13:37:34 -0700

    Updates

--- src/components/SectionHeader.jsx
+++
@@ -1,9 +0,0 @@
-const SectionHeader = ({ children }) => {
-    return (
-      <h1 className="text-6xl md:text-8xl font-thin text-white uppercase tracking-widest ">
-        {children}
-      </h1>
-    );
-  };
-
-  export default SectionHeader;
